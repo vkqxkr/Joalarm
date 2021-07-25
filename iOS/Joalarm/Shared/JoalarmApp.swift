75a1e8a10d894bab22093f36e90cfb1a6a820dc2
JinHyouk Park <vkqxkr@gmail.com>
2021-07-25 13:38:39 +0900

    iOS버전 추가

---
+++ iOS/Joalarm/Shared/JoalarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoalarmApp.swift
+//  Shared
+//
+//  Created by JinHyouk Park on 2021/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JoalarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
